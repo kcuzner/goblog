7f9c49ac30f206dcf44209cee4b2ffa4ec58e000
Kevin Cuzner <kevin@kevincuzner.com>
2014-07-24 15:54:05 -0600

    Added methods to add "path handlers" for arbitrary paths
    These take precendence over any normal routes, which is a bit of a
    security problem, but this is the quickest way.

--- site/blog/posts.go
+++ site/blog/posts.go
@@ -1,10 +1,22 @@
 package blog
 
 import (
+	"net/http"
 	"github.com/kcuzner/goblog/site"
-	"github.com/kcuzner/goblog/site/db"
+	//"github.com/kcuzner/goblog/site/db"
 )
 
+func feedGet(path string, w http.ResponseWriter, r *http.Request) bool {
+	println("feed?", path)
+	return false
+}
+
+func postGet(path string, w http.ResponseWriter, r *http.Request) bool {
+	println("post?", path)
+	return false
+}
+
 func init() {
-	
+	site.HandlePathFunc(feedGet)
+	site.HandlePathFunc(postGet)
 }
